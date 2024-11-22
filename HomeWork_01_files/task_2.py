ca54eea08e0e0955ee20c1274d9a80f2b6ab7653
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-11-22 19:44:00 +0300

    Доделал дз до конца

--- HomeWork_01_files/task_2.py
+++ HomeWork_01_files/task_2.py
@@ -8,11 +8,11 @@ def writes_text_to_file(text):
 writes_text_to_file('Если б мишки были пчелами,\nТо они бы нипочем,\nНикогда и не подумали,\nТак высоко строить дом.')
 
 
-def reads_the_text():
-    '''Функция считывает текст'''
-    with open('poem.txt', 'r', encoding='utf-8') as file:
+def reads_the_text(file):
+    '''Функция принимает файл и считывает текст'''
+    with open(file, 'r', encoding='utf-8') as file:
         text_list = file.readlines()
     return text_list
 
 
-print(''.join(reads_the_text()))
+print(''.join(reads_the_text('poem.txt')))
